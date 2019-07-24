17457a62baca6c25174fd14f47bafb6484653bed
Max Jiang <qj46@cornell.edu>
2019-07-24 14:13:47 -0400

    add experiment code for H2 chebyshev

--- python/chebyshev.py
+++ python/chebyshev.py
@@ -1,5 +1,6 @@
 import numpy as np
 from matplotlib import pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D
 
 from util import address_from_index
 from monomials import generate_T
@@ -104,54 +105,14 @@ def plot_h1_family(start, end, num_points, k, level = 7):
     return
 
 
-def extremal_val(poly_temp, num_points, flag, level = 7):
-    """
-    This function finds the points in a single polynomial, 
-        with one of the following properties:
-        (1) maximal value (2) minimal value (3) largest absolute value 
-        and returns their value and address.
-
-    Args:
-        poly_temp - evaluation of the polynomial to some level
-        num_points - the number of maximal/minimal points
-        flag - the kind of extremum we're trying to find. 1 represent 
-            maximal value, 2 represent minimal value, and 3 represent
-            maximal of absolute value.
-        level - The level we would like to evaluate 
-    
-    Returns:
-        Prints the top values of the address and value of extremal points
-    """
-
-    # If we need to find the sup-norm, take the absolute value
-    if (flag == 3):
-        poly_temp = np.abs(poly_temp)
-    
-    # If we need to find the maximum, we flip the sequence
-    if (flag == 1 or flag == 3):
-        ind_arrange = np.flip(np.argsort(poly_temp))
-    else: 
-        # If we need to find the minimum, no need to flip
-        ind_arrange = np.argsort(poly_temp)
-
-    # Find the top points
-    for i in range(num_points):
-        temp_add = address_from_index(level, ind_arrange[i]+1)
-        print("Address ", i)
-        print(temp_add)
-        print("Index", i)
-        print(ind_arrange[i])
-        print("Value ", i)
-        print(poly_temp[ind_arrange[i]])
-
-
 # Methods for finding value and position of multiple maximal points 
 #   for a single function in h1
 def extremal_val_h1(num_points, a, b, c, d, e, f, flag, level = 7):
     """
     This function finds the points in a single polynomial in H^{1}, 
         with one of the following properties:
-        (1) maximal value (2) minimal value (3) largest absolute value 
+        (1) maximal value (2) minimal value (3) largest absolute value
+        (4) minimum absolute value 
         and returns their value and address.
 
     Args:
@@ -231,8 +192,294 @@ def max_h1_val_family(start, end, num_points, k, level = 7):
         print('Maximum achieved at address ', max_add)
 
 
-def poly_sup_division(j1, k1, j2, k2, ):
+def extremal_val(poly_temp, num_points, flag, level = 7):
     """
-    This function calculates the 
+    This function finds the points in a single polynomial, 
+        with one of the following properties:
+        (1) maximal value (2) minimal value (3) largest absolute value 
+        and returns their value and address.
+
+    Args:
+        poly_temp - evaluation of the polynomial to some level
+        num_points - the number of maximal/minimal points
+        flag - the kind of extremum we're trying to find. 1 represent 
+            maximal value, 2 represent minimal value, and 3 represent
+            maximal of absolute value.
+        level - The level we would like to evaluate 
+    
+    Returns:
+        Prints the top values of the address and value of extremal points
     """
-    pass
+
+    # If we need to find the sup-norm, take the absolute value
+    if (flag == 3 or flag == 4):
+        poly_temp = np.abs(poly_temp)
+    
+    # If we need to find the maximum, we flip the sequence
+    if (flag == 1 or flag == 3):
+        ind_arrange = np.flip(np.argsort(poly_temp))
+    else: 
+        # If we need to find the minimum, no need to flip
+        ind_arrange = np.argsort(poly_temp)
+
+    # Find the top points
+    for i in range(num_points):
+        temp_add = address_from_index(level, ind_arrange[i]+1)
+        print("Address ", i)
+        print(temp_add)
+        print("Index", i)
+        print(ind_arrange[i])
+        print("Value ", i)
+        print(poly_temp[ind_arrange[i]])
+
+
+# A function to evaluate polynomials in H^{n}
+def eval_poly_hn(coeff_arr, level=7):
+    """
+    Function that evaluates the polynomial on H^{n}.
+
+    Args:
+        coeff_arr - n*3 ndarray storing the coefficients of the polynomial
+            coeff_arr[j, 0] represent coefficient of P_{j1}
+            coeff_arr[j, 1] represent coefficient of P_{j2}
+            coeff_arr[j, 2] represent coefficient of P_{j3}
+        level - The level we would like to evaluate
+    
+    Returns:
+        An array representing the values of the polynomial
+    """
+    # Fetch the highest power to evaluate
+    max_j = coeff_arr.shape[0]
+
+    # Evaluate all the polynomials
+    T = generate_T(level, max_j+2, frac=False)
+
+    poly_temp = np.zeros(T.shape[1])
+    for j in range(max_j):
+        for k in range(3):
+            poly_temp = poly_temp + coeff_arr[j, k] * T[k, :, j]
+
+    return poly_temp
+
+
+# Method for plotting any function in Hn given coefficients
+def plot_hn(coeff_arr, level = 7):
+    """
+    Function that plots a polynomial in H1. This follows from the 
+    evaluation function above.
+
+    Args:
+        coeff_arr - n*3 ndarray storing the coefficients of the polynomial
+            coeff_arr[j, 0] represent coefficient of P_{j1}
+            coeff_arr[j, 1] represent coefficient of P_{j2}
+            coeff_arr[j, 2] represent coefficient of P_{j3}
+        level - The level we would like to plot
+    
+    Returns:
+        A plot of the polynomial
+    """
+    poly_temp = eval_poly_hn(coeff_arr, level)
+    plot_general(poly_temp, level)
+
+
+# Methods for finding value and position of multiple maximal points 
+#   for a single function in H^{n}
+def extremal_val_hn(num_points, coeff_arr, flag, level = 7):
+    """
+    This function finds the points in a single polynomial in H^{1}, 
+        with one of the following properties:
+        (1) maximal value (2) minimal value (3) largest absolute value
+        (4) minimum absolute value 
+        and returns their value and address.
+
+    Args:
+        num_points - the number of maximal/minimal points
+        coeff_arr - n*3 ndarray storing the coefficients of the polynomial
+            coeff_arr[j, 0] represent coefficient of P_{j1}
+            coeff_arr[j, 1] represent coefficient of P_{j2}
+            coeff_arr[j, 2] represent coefficient of P_{j3}
+        flag - the kind of extremum we're trying to find. 1 represent 
+            maximal value, 2 represent minimal value, 3 represent
+            maximal of absolute value, 4 represent minimal of absolute
+            value.
+        level - The level we would like to evaluate 
+    
+    Returns:
+        Prints the top values of the address and value of extremal points
+    """
+
+    poly_temp = eval_poly_hn(coeff_arr, level)
+    extremal_val(poly_temp, num_points, flag, level)
+
+
+def max_h2_val_family(start0, end0, num0, start1, end1, num1, num_max, k, level=7):
+    T = generate_T(level, 3, frac=False)
+    P01_temp = T[0, :, 0]
+    P02_temp = T[1, :, 0]
+    P03_temp = T[2, :, 0]
+    P11_temp = T[0, :, 1]
+    P12_temp = T[1, :, 1]
+    P13_temp = T[2, :, 1]
+    P21_temp = T[0, :, 2]
+    P22_temp = T[1, :, 2]
+    P23_temp = T[2, :, 2]
+
+    index_0 = np.linspace(start0, end0, num0)
+    index_1 = np.linspace(start1, end1, num1)
+
+    xs = np.zeros((num0, num1))
+    ys = np.zeros_like(xs)
+    zs = np.zeros_like(xs)
+    pos_arr = np.zeros_like(xs) 
+
+
+    for i in range(num0):
+        for j in range(num1):
+            t0 = index_0[i]
+            t1 = index_1[j]
+
+            if (k == 1):
+                poly_temp = t0 * P01_temp + t1 * P11_temp + P21_temp
+            elif (k == 2):
+                poly_temp = t0 * P02_temp + t1 * P12_temp + P22_temp
+            else:
+                poly_temp = t0 * P03_temp + t1 * P13_temp + P23_temp
+            poly_temp = np.abs(poly_temp)
+            max_val = np.max(poly_temp)
+            max_pos = np.argmax(poly_temp)
+            max_add = address_from_index(level, max_pos+1)
+            xs[i, j] = t0
+            ys[i, j] = t1
+            zs[i, j] = max_val
+            pos_arr[i, j] = max_pos
+            
+            # print()
+            # print()
+            # print('Coefficient of P0k is ', t0)
+            # print('Coefficient of P1k is ', t1)
+            # print('Maximum value is', max_val)
+            # print('Maximum achieved at address ', max_add)
+    
+    # Reshape the arrays to have the right shape
+    xs = xs.reshape(num0*num1)
+    ys = ys.reshape(num0*num1)
+    zs = zs.reshape(num0*num1)
+    #print(zs)
+
+    # Sort the array based on the minimum max_norm
+    index = np.argsort(zs)
+    print(index)
+
+    # Find the best points
+    for i in range(num_max):
+        temp_ind = index[i]
+        
+        print("Coefficient of P0k with ranking ", i)
+        print(xs[temp_ind])
+        print("Coefficient of P1k with ranking ", i)
+        print(ys[temp_ind])
+        print("Value with ranking", i)
+        print(zs[temp_ind])
+        
+
+    # xs = xs.reshape(num0*num1)
+    # ys = ys.reshape(num0*num1)
+    # zs = zs.reshape(num0*num1)
+    # print(xs)
+    # print(ys)
+    # print(zs)
+
+    # fig = plt.figure()
+    # ax = fig.add_subplot(111, projection='3d')
+    # ax.scatter3D(xs, ys, zs)
+    # plt.show()
+    
+
+
+
+
+# def max_hn_val_family(n, range_list, num_points, k, level = 7):
+#     """
+#     Function that prints a list of supremum values for a certain family,
+#     given different coefficients.
+
+#     Args:
+#         n - The dimension of space H^{n} we're working with
+#         range_list - A 2*n ndarray, with the i-th column representing
+#             the lower and upper bound we search for the best value
+#         num_points - A n dim array, each entry represents the number of 
+#             points we would like to evaluate in a certain dimension
+#         k - The type of polynomial family (k=1,2,3)
+#         level - The level we would like to plot
+
+#     Returns:
+#         For each coefficient, prints out the address and value of 
+#             extremal points.
+#     """
+    
+#     T = generate_T(level, 3, frac=False)
+#     P01_temp = T[0, :, 0]
+#     P02_temp = T[1, :, 0]
+#     P03_temp = T[2, :, 0]
+#     P11_temp = T[0, :, 1]
+#     P12_temp = T[1, :, 1]
+#     P13_temp = T[2, :, 1]
+#     P21_temp = T[0, :, 2]
+#     P22_temp = T[1, :, 2]
+#     P23_temp = T[2, :, 2]
+
+#     index = np.linspace(start, end, num_points)
+
+#     for i in range(num_points):
+#         t = index[i]
+#         if (k == 1):
+#             poly_temp = t * P01_temp + P11_temp
+#         elif (k == 2):
+#             poly_temp = t * P02_temp + P12_temp
+#         else:
+#             poly_temp = t * P03_temp + P13_temp
+#         poly_temp = np.abs(poly_temp)
+#         max_val = np.max(poly_temp)
+#         max_pos = np.argmax(poly_temp)
+#         max_add = address_from_index(level, max_pos+1)
+
+#         print()
+#         print()
+#         print('Coefficient is ', t)
+#         print('Maximum value is', max_val)
+#         print('Maximum achieved at address ', max_add)
+
+
+#     num_points_total = np.prod(num_points)
+#     for i in range(num_points_total):
+#         # Fetch the index of the 
+
+    
+
+    
+#     for i in range(n):
+#         for j in range(num_points[i]):
+#             if (k == 1):
+#                 T[1,]
+
+
+
+#     for i in range(num_points):
+        
+#         t = index[i]
+#         if (k == 1):
+#             poly_temp = t * P01_temp + P11_temp
+#         elif (k == 2):
+#             poly_temp = t * P02_temp + P12_temp
+#         else:
+#             poly_temp = t * P03_temp + P13_temp
+#         poly_temp = np.abs(poly_temp)
+#         max_val = np.max(poly_temp)
+#         max_pos = np.argmax(poly_temp)
+#         max_add = address_from_index(level, max_pos+1)
+
+#         print()
+#         print()
+#         print('Coefficient is ', t)
+#         print('Maximum value is', max_val)
+#         print('Maximum achieved at address ', max_add)
--- python/plotting_test1.py
+++ python/plotting_test1.py
@@ -3,12 +3,15 @@ import sys
 import numpy as np
 import gmpy2 as gm
 
-from util import address_from_index
-from recursions import alpha, beta
-from monomials import p_jk, generate_W, generate_T, generate_norm_W, generate_norm_T, norm_p_jk, f_lkFiqn, norm_f_jk
-from plotting import plot_monomial, plot_easy_basis, plot_op, plot_general
-from chebyshev import eval_poly_h1, plot_h1, extremal_val, extremal_val_h1, max_h1_val_family
 np.set_printoptions(threshold=sys.maxsize)
+
+# Finding values of alpha, beta [useful in finding boundary values]
+from recursions import alpha, beta
+from monomials import p_jk, generate_W, generate_T, generate_norm_W, generate_norm_T
+from plotting import plot_monomial, plot_easy_basis, plot_op, plot_general
+from chebyshev import eval_poly_h1, plot_h1, extremal_val,  \
+    extremal_val_h1, max_h1_val_family, max_h2_val_family, plot_hn, \
+    eval_poly_hn, extremal_val_hn
 
 # An outline of the functionalities are listed here.
 # Section I. Plotting
@@ -28,21 +31,21 @@ np.set_printoptions(threshold=sys.maxsize)
 
 
 # 1-a. To plot the monomials, use plot_monomial
-#plot_monomial(num, k)
+# plot_monomial(num, k)
 
 
 # 1-b. To plot the easy basis, use plot_easy_basis
-#plot_easy_basis(num, k)
+# plot_easy_basis(num, k)
 
 
 # 1-c. This plots the Sobolov Orthogonal Polynomials
-#plot_SOP(num, k)
+# plot_op(num, k)
 
 
 ######## 2. Code for Plotting a general polynomial in H1 ########
 
 # Example: Compute P_{13} + 1/2*P_{03}
-#plot_h1(0, 0, 1/2, 0, 0, 1, 7)
+# plot_h1(0, 0, 1/2, 0, 0, 1, 7)
 
 
 
@@ -101,7 +104,85 @@ np.set_printoptions(threshold=sys.maxsize)
 # extremal_val(prop, num_points, flag=3, level=7)
 
 
-### Actual work 1: Sup norm of P_{13} + a * P_{03}
+
+######## 2. Code for finding Chebyshev Polynomials ########
+
+### Actual work 1: Chebyshev polynomial of order 1 with family 1
+# Goal: Find the minimal sup-norm of P_{11} + a * P_{01}
+# From a theoretical result, we know that a = -1/12 
+
+#plot_h1(-1/12, 0, 0, 1, 0, 0, 7)
+
+
+### Actual work 2: 
+# Goal: Find the minimal sup-norm of P_{12} + a * P_{02} 
+
+### Here we find the optimal value theore
+# # This indicates we evaluate 20 points one time.
+# num_points = 20
+
+# # Initial start and end values
+# start = float(-2 * beta(1) / beta(0))
+# end = 0
+# level = 7
+
+# # Second start and end values
+# start = -0.0748538
+# end = -0.0561403
+# level = 7
+
+# # Third start and end values
+# start = -0.0630347
+# end = -0.0610649
+# level = 7
+
+# # Fourth start and end values
+# start = -0.0621016
+# end = -0.0618943
+# level = 10
+
+# # Fifth start and end values
+# start = -0.0619488
+# end = -0.0619270
+# level = 10
+
+# # Sixth start and end values
+# start = -0.0619350
+# end = -0.0619327
+# level = 10
+
+# # Sixth start and end values
+# start = -0.0619340
+# end = -0.0619337
+# level = 10
+
+# # Print the maximal norm at each coefficient
+# max_h1_val_family(start, end, num_points, 2, level)
+
+# This implies the optimal coefficient is around -0.0619339
+# best_coeff = -0.0619339
+
+# Well, something we noted in the process is that the sup-norm seems
+# to be achieved at the point where the function has the same 
+# norm at F_0F_1q_2 and at q_1/q_2. 
+
+# extremal_val_h1(10, best_coeff, 0, 0, 1, 0, 0, 4, 7)
+
+# # Based on this conjecture, we can solve a linear equation that gives
+# # the exact value of a:
+# addr = [0, 1, 2]
+# addr = np.flip(addr)
+# addr = ''.join(str(int(x)) for x in addr)
+# best_coeff = -(p_jk(addr, 1, 2) + beta(1)) / (p_jk(addr, 0, 2) + beta(0))
+# print(float(best_coeff))
+# print(float(beta(1) + best_coeff * beta(0)))
+
+# # We finally print the graph of the final polynomial we obtain
+# plot_h1(0, best_coeff, 0, 0, 1, 0, 7)
+
+
+
+### Actual work 3: Sup norm of P_{13} + a * P_{03}
 
 # Initialization of the coefficients
 
@@ -148,86 +229,133 @@ np.set_printoptions(threshold=sys.maxsize)
 # # Print the maximal norm at each coefficient
 # max_h1_val_family(start, end, num_points, 3, level)
 
-# # This implies the optimal coefficient is around -0.02750235
+# This implies the optimal coefficient is around -0.02750235
 # best_coeff = -0.02750235
 
-# # We finally print the graph of the final polynomial we obtain
+# We finally print the graph of the final polynomial we obtain
 # plot_h1(0, 0, best_coeff, 0, 0, 1, 7)
 
+# extremal_val_h1(10, 0, 0, best_coeff, 0, 0, 1, 3, 7)
 
-### Actual work 2: Sup norm of P_{12} + a * P_{02}
 
-### Here we find the optimal value theore
+
+### Actual work 4: Sup norm of P_{21} + a * P_{11} + b * P_{01}
+
+# Initialization of the coefficients
+
+### Here we find the optimal value of second 
 # # This indicates we evaluate 20 points one time.
-# num_points = 20
+# num0 = 500
+# num1 = 500
 
 # # Initial start and end values
-# start = float(-2 * beta(1) / beta(0))
-# end = 0
+# start0 = -1/180
+# end0 = 1/180
+# start1 = -1/10
+# end1 = 1/30
+
 # level = 7
 
-# # Second start and end values
-# start = -0.0748538
-# end = -0.0561403
-# level = 7
+# max_h2_val_family(start0, end0, num0, start1, end1, num1, 10, 1, level)
 
-# # Third start and end values
-# start = -0.0630347
-# end = -0.0610649
-# level = 7
-
-# # Fourth start and end values
-# start = -0.0621016
-# end = -0.0618943
-# level = 10
-
-# # Fifth start and end values
-# start = -0.0619488
-# end = -0.0619270
-# level = 10
-
-# # Sixth start and end values
-# start = -0.0619350
-# end = -0.0619327
-# level = 10
-
-# # Sixth start and end values
-# start = -0.0619340
-# end = -0.0619337
-# level = 10
-
-# # Print the maximal norm at each coefficient
-# max_h1_val_family(start, end, num_points, 2, level)
-
-# # This implies the optimal coefficient is around -0.0619339
-# best_coeff = -0.0619339
-
-# Well, something we noted in the process is that the sup-norm seems
-# to be achieved at the point where the function has the same 
-# norm at F_0F_1q_2 and at q_1/q_2. 
-
-# Based on this conjecture, we can solve a linear equation that gives
-# the exact value of a:
-addr = [0, 1, 2]
-addr = np.flip(addr)
-addr = ''.join(str(int(x)) for x in addr)
-best_coeff = -(p_jk(addr, 1, 2) + beta(1)) / (p_jk(addr, 0, 2) + beta(0))
-print(float(best_coeff))
-print(float(beta(1) + best_coeff * beta(0)))
-
-# # We finally print the graph of the final polynomial we obtain
-# plot_h1(0, best_coeff, 0, 0, 1, 0, 7)
-
-#extremal_val_h1(10, 0, best_coeff, 0, 0, 1, 0, 3, 7)
-
-# ###
-# addr = [0, 1, 2]
+# 
+# addr = [1, 2]
 # addr = np.flip(addr)
 # addr = ''.join(str(int(x)) for x in addr)
+# print()
+# print()
+# print(p_jk(addr, 2, 1))
+# print(p_jk(addr, 1, 1))
 
-# print(p_jk(addr, 1, 2))
-# print(p_jk(addr, 0, 2))
-# # a = -(p_jk(addr, 1, 2) - beta(1)) / (p_jk(addr, 1, 2) - beta(0))
-# # or negative
-# a = -(p_jk(addr, 1, 2) + beta(1)) / (p_jk(addr, 0, 2) + beta(0))
-# print(a)
+
+
+# best_coeff = -(p_jk(addr, 1, 2) + beta(1)) / (p_jk(addr, 0, 2) + beta(0))
+# print(float(best_coeff))
+# print(float(beta(1) + best_coeff * beta(0)))
+
+# best_coeff_0 = 1 / 1125
+# best_coeff_1 = - 1 / 30
+
+# best_coeff_arr = np.zeros((3, 3))
+# best_coeff_arr[0, 0] = best_coeff_0
+# best_coeff_arr[1, 0] = best_coeff_1
+# best_coeff_arr[2, 0] = 1
+
+# plot_hn(best_coeff_arr, 7)
+# poly_temp = eval_poly_hn(best_coeff_arr, 7)
+# print(poly_temp[0])
+# extremal_val_hn(20, best_coeff_arr, 3, 7)
+# # TODO LATER, after we have the bounds.
+
+
+
+
+
+######## 3. Code for finding Optimality Conditions ########
+
+### Experiment 1: P_{03} / P_{11}
+
+# # Initiaize the parameters
+# j = 0
+# num_points = 10
+
+# # Generate the values of the polynomial
+# T = generate_T(7, j+2, False)
+# P01_seq = T[0, :, 0]
+# P02_seq = T[1, :, 0]
+# P03_seq = T[2, :, 0]
+# P11_seq = T[0, :, 1]
+# P12_seq = T[1, :, 1]
+# P13_seq = T[2, :, 1]
+
+# div_P03_P11 = np.divide(P03_seq, P11_seq)
+
+# Find the extremal value
+#extremal_val(div_P03_P11, num_points, flag=1, level=7)
+#plot_general(div_P03_P11, level=7)
+
+### Experiment 2: f_{10} + f_{11} + f_{12}
+
+# # Initiaize the parameters
+# j = 0
+# num_points = 10
+
+# # Generate the values of the polynomial
+# W = generate_W(7, j+2, False)
+# f00_seq = W[0, :, 0]
+# f01_seq = W[1, :, 0]
+# f02_seq = W[2, :, 0]
+# f10_seq = W[0, :, 1]
+# f11_seq = W[1, :, 1]
+# f12_seq = W[2, :, 1]
+
+# sum_f10_f11_f12 = f10_seq + f11_seq + f12_seq
+
+# # Find the extremal value
+# #extremal_val(div_P03_P11, num_points, flag=1, level=7)
+# plot_general(sum_f10_f11_f12, level=7)
+
+
+### Experiment 3: P_{01} / P_{21}
+
+# # Initiaize the parameters
+# j = 2
+# num_points = 10
+
+# # Generate the values of the polynomial
+# T = generate_T(7, j+2, False)
+# P01_seq = T[0, :, 0]
+# P11_seq = T[0, :, 1]
+# P21_seq = T[0, :, 2]
+
+# div_P21_P01 = np.divide(P21_seq, P01_seq)
+# div_P11_P01 = np.divide(P11_seq, P01_seq)
+
+# Find the extremal value
+# extremal_val(div_P21_P01, num_points, flag=2, level=7)
+# plot_general(div_P21_P01, level=7)
+
+# extremal_val(div_P11_P01, num_points, flag=1, level=7)
+# plot_general(div_P11_P01, level=7)
+
+# print(alpha(3))
