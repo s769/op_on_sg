948a8edd5a9b1a7e4cbb8bb169edbe4d9c4314f8
Sreeram Venkat <s769.098211@gmail.com>
2020-05-23 16:37:25 -0400

    fixed website

--- website/js/global.js
+++ website/js/global.js
@@ -3,66 +3,66 @@ $(document).ready(function () {
          
          
                     <div class="sidebar-header"> 
-                        <a href="/website/html/"> 
-                            <img class="img-fluid" src="/website/images/logo.png" alt="SG"> 
+                        <a href="/website/html/" class="sidelink"> 
+                            <img class="img-fluid" src="/website/images/logo.png" alt="Cornell Logo"> 
                         </a> 
                     </div> 
          
                     <ul class="list-unstyled components"> 
                         <li id="sidehome"> 
-                            <a href="/website/html/">Home</a> 
+                            <a href="/website/html/" class="sidelink">Home</a> 
                         </li> 
                         <li id="sidebg"> 
-                            <a href="#bgSubmenu" data-toggle="collapse" aria-expanded="false" >Background</a> 
+                            <a href="#bgSubmenu" data-toggle="collapse" aria-expanded="false" class="sidelink">Background</a> 
                             <ul class="collapse list-unstyled" id="bgSubmenu" data-parent="#sidebar"> 
                                 <li id="sideanalysis"> 
-                                    <a href="/website/html/background/analysis_on_fractals.html">Analysis on Fractals</a> 
+                                    <a href="/website/html/background/analysis_on_fractals.html" class="sidelink">Analysis on Fractals</a> 
                                 </li> 
                                 <li id="sidepoly"> 
-                                    <a href="/website/html/background/polynomials_on_sg.html">Polynomials on SG</a> 
+                                    <a href="/website/html/background/polynomials_on_sg.html" class="sidelink">Polynomials on SG</a> 
                                 </li> 
                             </ul> 
                         </li> 
          
                         <li id="sidetheory"> 
-                            <a href="#theorySubmenu" data-toggle="collapse" aria-expanded="false" >Theory</a> 
+                            <a href="#theorySubmenu" data-toggle="collapse" aria-expanded="false" class="sidelink">Theory</a> 
                             <ul class="collapse list-unstyled" id="theorySubmenu" data-parent="#sidebar"> 
                                 <li id="sideSOP"> 
-                                    <a href="/website/html/theory/sobolev_ops.html">Sobolev Orthogonal Polynomials</a> 
+                                    <a href="/website/html/theory/sobolev_ops.html" class="sidelink">Sobolev Orthogonal Polynomials</a> 
                                 </li> 
                                 <li id="sideCP"> 
-                                    <a href="/website/html/theory/chebyshev_ops.html">Chebyshev Polynomials</a> 
+                                    <a href="/website/html/theory/chebyshev_ops.html" class="sidelink">Chebyshev Polynomials</a> 
                                 </li> 
                             </ul> 
                         </li> 
                         <li id="sidenumerical"> 
-                            <a href="#numSubmenu" data-toggle="collapse" aria-expanded="false" >Numerical Results</a> 
+                            <a href="#numSubmenu" data-toggle="collapse" aria-expanded="false" class="sidelink">Numerical Results</a> 
                             <ul class="collapse list-unstyled" id="numSubmenu" data-parent="#sidebar"> 
                                 <li id="sideplot"> 
-                                    <a href="/website/html/numerical_results/plots.html">Plots</a> 
+                                    <a href="/website/html/numerical_results/plots.html" class="sidelink">Plots</a> 
                                 </li> 
                                 <li id="sidecomp"> 
-                                    <a href="/website/html/numerical_results/comparisons.html">Comparisons</a> 
+                                    <a href="/website/html/numerical_results/comparisons.html" class="sidelink">Comparisons</a> 
                                 </li> 
                                 <li id="sidezero"> 
-                                    <a href="/website/html/numerical_results/zeros.html">Zeros</a> 
+                                    <a href="/website/html/numerical_results/zeros.html" class="sidelink">Zeros</a> 
                                 </li> 
                                 <li id="sideapp"> 
-                                        <a href="/website/html/numerical_results/applications.html">Applications</a> 
+                                        <a href="/website/html/numerical_results/applications.html" class="sidelink">Applications</a> 
                                 </li> 
                             </ul> 
                         </li> 
                         <li id="siderec"> 
-                            <a href="#resourceSubmenu" data-toggle="collapse" aria-expanded="false" >Resources</a> 
+                            <a href="#resourceSubmenu" data-toggle="collapse" aria-expanded="false" class="sidelink">Resources</a> 
                             <ul class="collapse list-unstyled" id="resourceSubmenu" data-parent="#sidebar"> 
                                 <li id="sidecode"> 
-                                    <a href="/website/html/resources/programs.html">Software and Programs</a> 
+                                    <a href="/website/html/resources/programs.html" class="sidelink">Software and Programs</a> 
                                 </li> 
                                 <li id="sidepresent"> 
-                                    <a href="/website/html/resources/presentations.html">Presentations</a> 
+                                    <a href="/website/html/resources/presentations.html" class="sidelink">Presentations</a> 
                                 </li> 
                                 <li id="sideref"> 
-                                    <a href="/website/html/resources/references.html">References</a> 
+                                    <a href="/website/html/resources/references.html" class="sidelink">References</a> 
                                 </li> 
                             </ul> 
                         </li> 
@@ -87,8 +87,8 @@ $(document).ready(function () {
         var var_scroll = `<a id="back-to-top" href="#" class="btn btn-dark btn-lg back-to-top" role="button"><i class="fas fa-chevron-up"></i></a>`;
         var var_overlay = `<div class="overlay"></div>`
 
-        var var_button = `<button type="button" id="sidebarCollapse" class="btn btn-info pull-left">
-                                <i class="fas fa-align-justify"></i>
+        var var_button = `<button type="button" id="sidebarCollapse" class="btn btn-info pull-left"  style="margin-top:5px;">
+                                Menu <i class="fas fa-align-justify"></i>
                             </button>`;
 
     $(var_sidebar).prependTo($("#main"));
